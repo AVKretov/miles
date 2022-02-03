c800e5eb491c40db50b8bd65a017bc14f5fb47d2
Aleksandr Kretov <kretov_a@outlook.com>
2022-02-03 17:14:37 +0300

    Changes in milesprogramm value

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int ticketPrice = 800;
-        int milesPerRuble = 13;
+        int milesPerRuble = 20;
         int miles = ticketPrice/milesPerRuble;
 
         System.out.println("Вы заработали "+ miles + " миль с этой покупки, поздравляем!");
